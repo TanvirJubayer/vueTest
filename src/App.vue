4694da89d79eac87c246c258125e7d8eb3343626
Tanvir_Jobayer <tnvirjubayer96@gmail.com>
2026-01-27 11:05:41 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+
+<script setup>
+import { ref } from "vue";
+import Counter from "./components/counter.vue";
+
+  let Name="Masud";
+  let Address= "Segun Bagicha, Dhaka";
+  let html="<span>Hello BD</span>";
+
+  let active =ref(false);
+  const changeName = ()=>{
+    active.value = !active.value;
+  }
+
+  // Array
+  let Students = ["Jamal","Kamal","Tomal","Rasel", "Arif"];
+  
+</script>
+
+
+<template>
+  <h1>Hello, {{ Name }}</h1>
+  <p>Address: {{ Address }}</p>
+  <h1>Oyy... <span v-text="Name"></span> !!!</h1>
+  <h2 v-html="html"></h2>
+
+  <br><br>
+  <br>
+
+  <h3 v-if="active">Hashao Mahmud !</h3>
+  <h3 v-else>Mahmud, hashi kothay?</h3>
+  <button v-on:click="changeName">Change-Name</button>
+
+  <br><br>
+  <br><br>
+  <ol>
+    <li v-for="(student , i) in Students" :key="i">{{ student }}</li>
+  </ol>
+  <br><br>
+  <br>
+
+  <Counter/>
+  <br>
+  <br>
+  <Counter/>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+    
+  <!-- Counter -->
+  <div class="counter">
+    <h2>Counter: {{ count }}</h2>
+
+    <button @click="decrease">−</button>
+    <button @click="increase">+</button>
+    <button @click="reset">Reset</button>
+  </div>
+
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+
+
+  let count = ref(0);
+  function increase(){
+    count.value++;
+  }
+  
+function decrease() {
+  count.value--;
+}
+
+function reset() {
+  count.value = 0;
+}
+</script>
+
+<style>
+
+</style>
